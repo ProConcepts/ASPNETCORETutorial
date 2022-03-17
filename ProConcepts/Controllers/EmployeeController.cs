d8d0c7e69f46fbd19d9756a19e6ddd8e4ba81bef
poojaparashar36 <55698852+poojaparashar36@users.noreply.github.com>
2022-03-17 17:26:31 +0530

    First Commit

---
+++ ProConcepts/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ProConcepts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProConcepts.Controllers
+{
+    
+    public class EmployeeController : Controller
+    {
+        public IEmployeeRepository _repository;
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+        public ViewResult Index()
+        {
+            List<Employee> employees = _repository.GetEmployees();
+            return View(employees);
+        }
+        
+        public string DeleteEmployee(int id)
+        {
+            return "Controller = Empoloyee and Action = DeleteEmployee";
+        }
+    }
+}
---
+++ ProConcepts/Models/SQLDBLayer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProConcepts.Models
+{
+    public class SQLDBLayer : IEmployeeRepository
+    {
+        public IConfiguration Configuration { get; }
+        public SQLDBLayer(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection con = new SqlConnection(Configuration.GetConnectionString("EmployeeContext")))
+            {
+                string query = "SELECT ID, NAME, SALARY, A.DEPARTMENT, DEPT_NAME FROM TBLEMPLOYEE A " +
+                    "LEFT JOIN TBLDEPARTMENT B ON A.DEPARTMENT = B.DEPT_ID;";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            employees.Add(new Employee
+                            {
+                                Id= Convert.ToInt32(sdr["id"]),
+                                Name=sdr["name"].ToString(),
+                                Salary = Convert.ToInt32(sdr["Salary"]),
+                                Department = Convert.ToInt32(sdr["Department"])
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            return employees;
+        }
+    }
+}
